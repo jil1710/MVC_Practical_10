2444545baee8519ff0097f9a7405fcbd7e29df68
rider <jil.p@simformsolutions.com>
2023-06-28 15:58:30 +0530

    add: exception divide by zero handling

---
+++ ExceptionHandling/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExceptionHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HandleError(ExceptionType = typeof(DivideByZeroException), View = "DivideByZeroException")]
+        public ActionResult Index()
+        {
+            int a = 0;
+            int b = 10;
+            int c = b / a;
+            ViewBag.Result = c;
+            return View();
+        }
+    }
+}
